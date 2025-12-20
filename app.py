d709c15323dd8b81dd9b84c7ed03eb584770418b
DH2T <dh2tco@gmail.com>
2025-12-20 23:07:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if "reset_trigger" not in st.session_state:
 # --- 5. GIAO DIỆN CHÍNH ---
 
 if not st.session_state.found:
-    st.header("⚡ TRA CỨU BẢO HÀNH")
+    st.header("⚡TRA CỨU BẢO HÀNH")
     st.write("Biến Áp Minh Quang - CTy DH2T")
     
     # Khu vực Quét QR - Sử dụng reset_trigger để làm mới scanner khi tra mã khác
@@ -104,5 +104,6 @@ else:
     st.link_button("📞 Gọi hỗ trợ: 0903.736.414", "tel:0903736414", use_container_width=True, type="primary")
 
 st.sidebar.page_link("https://bienapminhquang.com", label="Quay lại Website", icon="🏠")
+
 
 
